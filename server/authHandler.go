f77cd496e05a101c96c017baaa2f2f926c38e875
Nikolay Tonev <nikolay.tonev@gmail.com>
2021-06-17 23:03:23 +0200

    yet another url

--- server/authHandler.go
+++ server/authHandler.go
@@ -163,7 +163,7 @@ func (h *authHandler) initAuthCode(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: save the pair auth-code & state in the database
 	code := ksuid.New().String()
-	rurl := "/auth/login"
+	rurl := "https://ivmauth-staging-xmywgxnrfq-ez.a.run.app/auth/login"
 
 	ru := fmt.Sprintf("%s?code=%s&state=%s", rurl, code, q.Get("state"))
 
