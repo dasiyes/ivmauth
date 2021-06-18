e626ff9a0a24b6d48f844162b9af4bda4a5dc386
Nikolay Tonev <nikolay.tonev@gmail.com>
2021-06-18 21:03:22 +0200

    change redirect url

--- server/authHandler.go
+++ server/authHandler.go
@@ -163,7 +163,7 @@ func (h *authHandler) initAuthCode(w http.ResponseWriter, r *http.Request) {
 
 	// TODO: save the pair auth-code & state in the database
 	code := ksuid.New().String()
-	rurl := "https://ivmauth-staging-xmywgxnrfq-ez.a.run.app/auth/login"
+	rurl := "/auth"
 
 	ru := fmt.Sprintf("%s?code=%s&state=%s", rurl, code, q.Get("state"))
 
