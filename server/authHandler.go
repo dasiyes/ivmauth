af2b8874fbd3be1122909820fc9626c25727a833
Nikolay Tonev <nikolay.tonev@gmail.com>
2021-06-21 22:20:03 +0200

    change login screen

--- server/authHandler.go
+++ server/authHandler.go
@@ -162,6 +162,8 @@ func (h *authHandler) initAuthCode(w http.ResponseWriter, r *http.Request) {
 	_ = level.Debug(h.logger).Log("GET-/auth", r.URL.RawQuery, "state", q.Get("state"))
 
 	// TODO: save the pair auth-code & state in the database
+	h.aus.RegisterNewRequest()
+
 	code := ksuid.New().String()
 	rurl := "/auth"
 
