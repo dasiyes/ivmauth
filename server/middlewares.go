8d33efb9cfe214da1906bec03948faeec837fc0b
Nikolay Tonev <nikolay.tonev@gmail.com>
2021-12-20 17:19:55 +0100

    fix white list endpoints

--- server/middlewares.go
+++ server/middlewares.go
@@ -109,6 +109,10 @@ func isReqWhitelisted(r *http.Request) bool {
 		return true
 	case "POST /oauth/register":
 		return true
+	case "GET oauth/ui/activate":
+		return true
+	case "GET oauth/activate":
+		return true
 	case "GET /oauth/ui/login":
 		return true
 	case "GET /oauth/ui/register":
