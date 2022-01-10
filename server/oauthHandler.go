15516787e8ca64f5b9788b636b90a474360b1756
Nikolay Tonev <nikolay.tonev@gmail.com>
2022-01-10 20:33:23 +0100

    set ia Path to root

--- server/oauthHandler.go
+++ server/oauthHandler.go
@@ -529,7 +529,7 @@ func (h *oauthHandler) handleAuthCodeFlow(
 		return
 	}
 
-	w.Header().Add("Set-Cookie", "ia=1; HTTPOnly")
+	w.Header().Add("Set-Cookie", "ia=1; HTTPOnly; Path=/")
 
 	// redirect back to web app page (registered for the client id)
 	http.Redirect(w, r, rb.RedirectUri, http.StatusSeeOther)
